e6102d80002ea72757425023e3caf08459623d58
Matias Ariel Coco <91424961+matiasCoco1997@users.noreply.github.com>
2025-08-28 15:29:57 -0300

    proyectos

--- src/components/proyectos.tsx
+++ src/components/proyectos.tsx
@@ -4,81 +4,64 @@ const proyectos = [
   {
     id: 1,
     title: "Ingenierías y auditorias",
-    img: "/home/proyectos/",
-    icon: "fas fa-star",
-    color: "bg-blue-500",
+    img: "/home/proyectos/pencil.webp",
   },
   {
     id: 2,
     title: "Servicios de mantenimiento",
-    img: "/home/proyectos/",
-    icon: "fas fa-heart",
-    color: "bg-green-500",
+    img: "/home/proyectos/tool.webp",
   },
   {
     id: 3,
     title: "Shelters sala de bombas",
     img: "/home/proyectos/shelter-icon.webp",
-    icon: "fas fa-moon",
-    color: "bg-purple-500",
   },
   {
     id: 4,
     title: "Skids sala de bombas",
     img: "/home/proyectos/skids-icon.webp",
-    icon: "fas fa-sun",
-    color: "bg-red-500",
   },
   {
     id: 5,
     title: "Sistemas de extinción por agua",
-    img: "/home/proyectos/",
-    icon: "fas fa-cloud",
-    color: "bg-yellow-500",
+    img: "/home/proyectos/water.webp",
   },
   {
     id: 6,
     title: "Sistemas de extinción por espuma",
-    img: "/home/proyectos/",
-    icon: "fas fa-bolt",
-    color: "bg-indigo-500",
+    img: "/home/proyectos/foam-icon.webp",
   },
   {
     id: 7,
     title: "Sistemas de detección y aviso de incendio",
     img: "/home/proyectos/detection-icon.webp",
-    icon: "fas fa-leaf",
-    color: "bg-pink-500",
   },
   {
     id: 8,
     title: "Sistemas de extinción por gases",
     img: "/home/proyectos/co2-icon.webp",
-    icon: "fas fa-gem",
-    color: "bg-teal-500",
   },
 ];
 
 export default function Proyectos() {
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4 pb-8 justify-items-center xl:gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-8 pb-8 justify-items-center xl:gap-6">
         {proyectos.map((proyecto) => (
           <Link
             href={`/proyectos/${proyecto.id}`}
             key={proyecto.id}
-            className={`p-6 rounded-lg shadow-lg flex flex-col items-center justify-center bg-white h-80 w-80 xl:w-60 xl:h-60 hover:scale-105 transition-transform duration-200`}
+            className={`p-6 rounded-lg shadow-lg flex flex-col items-center justify-center bg-white h-70 w-70 xl:w-70 xl:h-60 hover:scale-105 transition-transform duration-200`}
           >
             <div className="flex flex-col items-center justify-center w-full h-full text-epix-500">
               <div className="flex items-center justify-center border-2 border-epix-300 rounded-full w-20 h-20 bg-red-100 p-3">
                 <img
                   src={proyecto.img}
                   alt={proyecto.title}
-                  className="w-10 h-auto max-h-9"
+                  className="w-auto h-8 max-w-10"
                   style={{ objectFit: "contain" }}
                 />
               </div>
-
               <p className="p-3 font-semibold text-center text-xl">
                 {proyecto.title}
               </p>
