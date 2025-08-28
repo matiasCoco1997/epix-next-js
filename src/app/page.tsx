0bed9de78d93d8cf9c4f2b95a2f52d0f3d11e8f4
Matias Ariel Coco <91424961+matiasCoco1997@users.noreply.github.com>
2025-08-28 16:07:50 -0300

    Add certifications section with images to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Proyectos from "@/components/proyectos";
 import Carousel from "@/components/ui/carousel";
+import Image from "next/image";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -42,6 +43,66 @@ export default function Home() {
           </h1>
 
           <Proyectos />
+        </article>
+      </section>
+
+      <section className="container mx-auto">
+        <article className="py-8">
+          <h2 className="text-center py-8 text-epix-500 text-2xl font-bold title-font mx-5 md:mx-0">
+            Certificaciones
+          </h2>
+
+          <ul className="flex justify-between items-center w-[90%] mx-auto">
+            <li>
+              <Image
+                src="/home/certificaciones/iram.webp"
+                alt="Certificación 3"
+                width={100}
+                height={100}
+                className="w-12 md:w-20"
+              />
+            </li>
+
+            <li>
+              <Image
+                src="/home/certificaciones/agc.webp"
+                alt="Certificación 1"
+                width={100}
+                height={100}
+                className="w-12 md:w-18"
+              />
+            </li>
+
+            <li>
+              <Image
+                src="/home/certificaciones/copime.webp"
+                alt="Certificación 2"
+                width={100}
+                height={100}
+                className="w-12 md:w-18"
+              />
+            </li>
+
+            <li>
+              <Image
+                src="/home/certificaciones/nfpa.webp"
+                alt="Certificación 4"
+                width={100}
+                height={100}
+                className="w-12 md:w-18"
+              />
+            </li>
+
+            <li>
+              <Image
+                src="/home/certificaciones/iram.webp"
+                alt="Certificación 3"
+                width={100}
+                height={100}
+                className="w-12 md:w-20"
+              />
+            </li>
+          </ul>
         </article>
       </section>
     </>
